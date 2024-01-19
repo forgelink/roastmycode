09a618dfcb7a7a5ded3c05be2907e0e995e721e9
Mehrab <white_joker20@yahoo.com>
2024-01-19 23:59:25 +0330

    fix: make hero responsive

--- resources/js/Components/HeroSection.jsx
+++ resources/js/Components/HeroSection.jsx
@@ -2,9 +2,9 @@ import SecondaryButton from "./SecondaryButton";
 
 export default function HeroSection() {
     return (
-        <div className="h-[100vh] overflow-hidden">
-            <div className="flex flex-col lg:flex-row items-center justify-between container relative h-full">
-                <div>
+        <div className="h-[145vh] lg:h-[100vh] overflow-hidden">
+            <div className="flex flex-col lg:flex-row items-center lg:justify-between container relative h-full">
+                <div className="mt-32">
                     <h1 className='text-6xl font-black'>
                         Talk is cheap.
                         <br />
@@ -21,11 +21,11 @@ export default function HeroSection() {
                     </SecondaryButton>
                 </div>
 
-                <div className="">
-                    <div className="absolute top-50 right-56 w-[200px] h-56 bg-[#FFB86C] rounded-full blur-[500px]" />
-                    <div className="absolute top-96 right-56 w-[200px] h-56 bg-[#FF79C6] rounded-full blur-[500px]" />
-                    <div className="absolute top-72 right-20 w-[100px] h-56 bg-[#50FA7B] rounded-full blur-[500px]" />
-                    <img className="w-[550px] z-10 relative"
+                <div className="mt-10">
+                    <div className="absolute -z-10 top-50 right-56 w-[200px] h-56 bg-[#FFB86C] rounded-full blur-[500px]" />
+                    <div className="absolute -z-10 top-96 right-56 w-[200px] h-56 bg-[#FF79C6] rounded-full blur-[500px]" />
+                    <div className="absolute -z-10 top-72 right-20 w-[100px] h-56 bg-[#50FA7B] rounded-full blur-[500px]" />
+                    <img className="w-[550px]"
                         src='/hero-image.png' alt="Roast my Code" />
                 </div>
             </div>
