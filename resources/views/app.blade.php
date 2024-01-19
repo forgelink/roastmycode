ba8455de3c4f013e0ef96b0e222367d5749bc484
Mehrab <white_joker20@yahoo.com>
2024-01-20 00:39:12 +0330

    fix: add initial loading

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,10 @@
 
     <title inertia>{{ config('app.name', 'Roast My Code') }}</title>
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+
     <!-- Scripts -->
     @routes
     @viteReactRefresh
@@ -16,6 +20,19 @@
 
 <body class="font-sans antialiased bg-[#282A36] text-white">
     @inertia
+
+    <div class="top-0 right-0 left-0 bottom-0 fixed flex items-center justify-center z-50" id="initial_loading">
+        <span class="loading mx-auto"></span>
+    </div>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var loadingElement = document.getElementById('initial_loading');
+
+            if (loadingElement) {
+                loadingElement.remove();
+            }
+        });
+    </script>
 </body>
 
 </html>
