4624ea503ceff29c7c249192627b5565e69da3f6
Mehrab <white_joker20@yahoo.com>
2024-01-20 20:10:06 +0330

    feat: create model & migration for posts

---
+++ app/Models/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public $fillable = ['parent_id', 'user_id', 'content', 'language', 'code'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
