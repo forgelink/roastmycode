69b110f3ec3263d2221f59d0864b152cd28195cb
Mehrab <white_joker20@yahoo.com>
2024-01-24 21:54:06 +0330

    fix: disable cursor

--- resources/js/Components/SecondaryButton.jsx
+++ resources/js/Components/SecondaryButton.jsx
@@ -4,7 +4,7 @@ export default function SecondaryButton({ type = 'button', className = '', disab
             {...props}
             type={type}
             className={
-                `inline-flex items-center px-4 py-2 bg-[#F8F8F2] hover:bg-[#e0e0dc] rounded-xl font-semibold text-xs text-gray-700 shadow-sm disabled:opacity-25 transition ease-in-out duration-150 ${disabled && 'opacity-25'
+                `inline-flex items-center px-4 py-2 bg-[#F8F8F2] hover:bg-[#e0e0dc] rounded-xl font-semibold text-xs text-gray-700 shadow-sm transition ease-in-out duration-150 ${disabled && 'opacity-25 cursor-not-allowed'
                 } ` + className
             }
             disabled={disabled}
